ba2a31633c4cd1034ebcb64369aa91a289f231a2
KhalilC <klcrumpler@gmail.com>
2013-06-07 03:53:07 -0400

    added 5th quiz

---
+++ lesson2ex5.py
@@ -0,0 +1,11 @@
+# Define a procedure, bigger, that takes in
+# two numbers as inputs, and returns the
+# greater of the two inputs.
+
+def bigger(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+        
