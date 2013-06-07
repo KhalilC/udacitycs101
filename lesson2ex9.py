ca7fdc4dc27d56a8a795f25032d4359101ecd8d7
KhalilC <klcrumpler@gmail.com>
2013-06-07 06:03:08 -0400

    added 9th quiz

---
+++ lesson2ex9.py
@@ -0,0 +1,23 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+def print_numbers(num):
+    start = 0
+    while start != num:
+        start = start + 1
+        print start
+    
+                  
+   
+
+
+
+#print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
